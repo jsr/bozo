66d31f648b63686708cb2aac6ed27e7af8b5b6f7
Mikin Faldu <Mikin@Mikins-MacBook.local>
2011-02-17 15:49:02 -0800

    - do not auto-refresh when list boxes have focus

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,25 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+if (typeof (TENGEN) === "undefined" || !TENGEN) {
+  var TENGEN = {};
+}
+
+TENGEN.util = {};
+
+TENGEN.util.Doc = {
+  reloadPage: function(abortIfFormElemHasFocus) {
+    var reload = true;
+    var activeElement = document.activeElement.tagName.toUpperCase();
+
+    if ((activeElement == 'SELECT') || activeElement == 'INPUT' || activeElement == 'TEXTAREA') {
+      if (abortIfFormElemHasFocus) {
+        reload = false;
+      }
+    }
+
+    if (reload) {
+      location.reload();
+    }
+  }
+};
